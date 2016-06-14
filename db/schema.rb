5183d556b63d07be4f48837f2b2105c5c77655ba
Scott Macfarlane <scott@gina.alaska.edu>
2016-06-13 17:34:43 -0800

    Add source to artwork

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613190029) do
+ActiveRecord::Schema.define(version: 20160614013404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20160613190029) do
     t.string   "status",     default: "pending", null: false
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.integer  "source_id"
+    t.index ["source_id"], name: "index_artworks_on_source_id", using: :btree
+  end
+
+  create_table "sources", force: :cascade do |t|
+    t.string   "image_id"
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +65,5 @@ ActiveRecord::Schema.define(version: 20160613190029) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "artworks", "sources"
 end
